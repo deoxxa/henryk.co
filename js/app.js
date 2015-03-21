ed6b642bfee718abf4a3fe50d5035167339009d7
deoxxa <deoxxa@fknsrs.biz>
2015-03-21 11:38:47 +1100

    initial revision of site

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var ajaxpCounter = 0;
+var ajaxp = function ajaxp(url, cb) {
+  var k = "ajaxp_" + ajaxpCounter++;
+
+  window[k] = function(r) {
+    delete window[k];
+    cb(r);
+  };
+
+  var s = document.createElement("script");
+  if (url.indexOf("?") === -1) {
+    s.src = url + "?callback=" + k;
+  } else {
+    s.src = url + "&callback=" + k;
+  }
+
+  document.getElementsByTagName("script")[0].parentNode.appendChild(s);
+};
+
+(function() {
+  var userId = 1777759803;
+  var accessToken = "1777759803.467ede5.ff9d3f78879b44c6b9205467bfe7ef15";
+
+  var url = "https://api.instagram.com/v1/users/" + userId + "/media/recent/?access_token=" + accessToken;
+
+  ajaxp(url, function(r) {
+    if (!r) {
+      return;
+    }
+
+    document.getElementById("photo").src = r.data[0].images.standard_resolution.url;
+  });
+})();
